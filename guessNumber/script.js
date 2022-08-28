3a5158c8ea8c31840bb68a9c322beca9b4185ee4
Kulin Rangani <kulinrangani01@gmail.com>
2022-08-28 14:38:14 +0530

    Code refactored

--- guessNumber/script.js
+++ guessNumber/script.js
@@ -3,16 +3,20 @@ console.log(ans);
 let score = 20;
 let highscore = 0;
 
+const displaymessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   document.querySelector(".score").textContent = score;
-  document.querySelector(".message").textContent = "Start guessing...";
   ans = Math.trunc(Math.random() * 20 + 1);
   console.log(ans);
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
   document.querySelector(".number").textContent = "?";
+  displaymessage("Start guessing...");
 });
 
 document.querySelector(".check").addEventListener("click", function () {
@@ -22,24 +26,22 @@ document.querySelector(".check").addEventListener("click", function () {
     if (!guess) {
       document.querySelector(".message").textContent = "No number 🙅‍♀️";
     } else if (guess === ans) {
-      document.querySelector(".message").textContent = "Correct Number 🎉";
       document.querySelector(".number").textContent = ans;
       document.querySelector("body").style.backgroundColor = "gray";
       document.querySelector(".number").style.width = "30rem";
+      displaymessage("Correct Number 🎉");
       if (score > highscore) {
         highscore = score;
         document.querySelector(".highscore").textContent = score;
       }
-    } else if (guess > ans) {
+    } else if (guess !== ans) {
       score--;
-      document.querySelector(".message").textContent = "📈 High Number Guessed";
-      document.querySelector(".score").textContent = score;
-    } else if (guess < ans) {
-      score--;
-      document.querySelector(".message").textContent = "📉 Low Number Guessed";
+      displaymessage(
+        guess > ans ? "📈 High Number Guessed" : "📉 Low Number Guessed"
+      );
       document.querySelector(".score").textContent = score;
     }
   } else if (score == 0) {
-    document.querySelector(".message").textContent = "☹ You lost the game!";
+    displaymessage("☹ You lost the game!");
   }
 });
